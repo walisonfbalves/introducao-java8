406afc750307c10c195db28f6ee901f127d03165
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-13 13:15:11 -0300

    Inkjeção de depdependências

--- src/main/java/io/github/walisonfbalves/repository/ClientesRespository.java
+++ src/main/java/io/github/walisonfbalves/repository/ClientesRespository.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClientesRespository {
+
+    public void persistir(Object cliente) {
+        //acessa a base e salva o cliente
+    }
 }
--- src/main/java/io/github/walisonfbalves/service/ClientesServices.java
+++ src/main/java/io/github/walisonfbalves/service/ClientesServices.java
@@ -1,8 +1,26 @@
 package io.github.walisonfbalves.service;
 
+import io.github.walisonfbalves.repository.ClientesRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ClientesServices {
+
+    private ClientesRespository repository;
+
+    @Autowired
+    public ClientesServices (ClientesRespository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarCliente(Object cliente) {
+        validarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void validarCliente(Object cliente) {
+        //aplica validações
+    }
 }
