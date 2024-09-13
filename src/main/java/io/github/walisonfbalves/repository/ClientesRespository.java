3ec307ae873eb6a442b4852906343b83a3d6f98a
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-13 12:46:27 -0300

    Fazendo o backend, @configuration e @bean

---
+++ src/main/java/io/github/walisonfbalves/repository/ClientesRespository.java
@@ -0,0 +1,7 @@
+package io.github.walisonfbalves.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRespository {
+}
---
+++ src/main/java/io/github/walisonfbalves/service/ClientesServices.java
@@ -0,0 +1,8 @@
+package io.github.walisonfbalves.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ClientesServices {
+}
