1730a0e8bc97a1cf72044bc70f8c09ee4c8a6734
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-14 12:15:39 -0300

    Trabalhando com multiplos ambientes e profiles

--- src/main/java/io/github/walisonfbalves/MinhaConfiguration.java
+++ src/main/java/io/github/walisonfbalves/MinhaConfiguration.java
@@ -1,14 +1,19 @@
 package io.github.walisonfbalves;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("production")
 public class MinhaConfiguration {
 
-    @Bean(name="applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Executando no sitema de development...");
+        };
     }
 }
